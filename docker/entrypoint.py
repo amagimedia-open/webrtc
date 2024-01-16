85b21acd4ee3be6deff7b265b8906a898a5eef2f
yvvarun <yv.varun@amagi.com>
2024-01-16 10:44:19 +0000

    dockerfile fixes

---
+++ docker/entrypoint.py
@@ -0,0 +1,7 @@
+import subprocess
+
+# Run discoverConfigs.py
+subprocess.run(['python3', '/home/root/installation/config_discovery/discoverConfigs.py'])
+
+# Run main.py
+subprocess.run(['python3', '/home/root/installation/webrtc_server/janus/app/main.py'])
